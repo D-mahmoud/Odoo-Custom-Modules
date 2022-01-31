63abeee7edfd6fe9114e7f04ceabbfbaa5c8a025
developer <developer@developer.localdomain>
2022-01-31 17:13:59 +0200

    omar custom

---
+++ gold_price/models/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class goldPrice(models.Model):
+    _name = 'gold.price'
+    _description = 'gold price'
+
+    gold_price = fields.Float(string='Gold Price' ,required=True,help="set gold price to day")
+    today = fields.Datetime(string='Gold Price Date', required=True, default= fields.datetime.now())
+    name = fields.Char(string="Name", required=False, help="Write your name")
+
+   
