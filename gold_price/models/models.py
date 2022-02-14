b0589786903a2230e627757725adadee3b6afa34
D-mahmoud <dalia1711696@miuegypt.edu.eg>
2022-02-14 12:36:37 +0200

    My message

--- gold_price/models/models.py
+++ gold_price/models/models.py
@@ -4,9 +4,9 @@ from odoo import models, fields, api
 class goldPrice(models.Model):
     _name = 'gold.price'
     _description = 'gold price'
-
+    karat = fields.Integer(string="Karat", required=True)
     gold_price = fields.Float(string='Gold Price' ,required=True,help="set gold price to day")
-    today = fields.Datetime(string='Gold Price Date', required=True, default= fields.datetime.now())
-    name = fields.Char(string="Name", required=False, help="Write your name")
+    day = fields.Date(string="Active From Date",required=True)
+    
 
    
